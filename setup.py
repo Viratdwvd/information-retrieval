e00d98d57fe3bfa4827f48845b974711cb2f12e3
Virat <viratproject1@gmail.com>
2025-06-12 18:20:19 +0530

    all other files code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Virat Dwivedi',
+    author_email= 'vrtdwvd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
